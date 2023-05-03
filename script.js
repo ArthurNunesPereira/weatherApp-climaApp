0d3cc1c8fac2b444ad8084499387ce94c3b735bc
Arthur Nunes Pereira <arthurnunesp@live.com>
2023-05-03 12:16:15 -0300

    commit final?

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const key = "e1d762e0204cd549df4686cfdfd4cf6d"
 function getDataOnScreen(data) {
     console.log(data)
     document.querySelector(".city").innerHTML = "Weather in " + data.name
-    document.querySelector(".temp").innerHTML = data.main.temp + " Cº."
+    document.querySelector(".temp").innerHTML = Math.floor(data.main.temp) + " Cº."
     document.querySelector(".text-weather-mood").innerHTML = data.weather[0].description + "."
     document.querySelector(".text-humidity").innerHTML = data.main.humidity + " %."
+    document.querySelector(".bx-cloud").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
 }
 
 async function searchCity(city) {
