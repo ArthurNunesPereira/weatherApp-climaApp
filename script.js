2fed1a098c0d733f4143946df3a77b46ad74e04f
Arthur Nunes Pereira <arthurnunesp@live.com>
2023-05-03 10:46:19 -0300

    commit erro JS resolvido

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const key = "e1d762e0204cd549df4686cfdfd4cf6d"
 
+function getDataOnScreen(data) {
+    console.log(data)
+}
+
 async function searchCity(city) {
     const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`).then(response => response.json())
 
-    console.log(data)
+    getDataOnScreen(data)
 }
 
 function searchClick() {
@@ -11,4 +15,3 @@ function searchClick() {
 
     searchCity(city)
 }
-
