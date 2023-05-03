5882ec727e049aea2516b60aa8f6ca643b6c60ac
Arthur Nunes Pereira <arthurnunesp@live.com>
2023-05-03 10:03:43 -0300

    commit JS

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// window.alert("Hello World!")
+const key = "e1d762e0204cd549df4686cfdfd4cf6d"
+
+async function searchCity(city) {
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`).then(response => response.json())
+
+    console.log(data)
+}
+
+function searchClick() {
+    const city = document.querySelector(".input-city").value
+
+    searchCity(city)
+}
+
