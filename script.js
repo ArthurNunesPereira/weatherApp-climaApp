dd3cd9afd417fd1664dc2ca1d2ae6e2b1cff80f3
Arthur Nunes Pereira <arthurnunesp@live.com>
2023-05-04 12:30:26 -0300

    commid flag

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const key = "e1d762e0204cd549df4686cfdfd4cf6d"
+const countryElement = document.querySelector("#country");
 
-function getDataOnScreen(data) {
+async function getDataOnScreen (data) { // Esse teria q ser async tbm...
     try {
+        const apiCountryURL = `https://www.countryflagicons.com/FLAT/64/${data.sys.country}.png`;
         document.querySelector(".city").innerHTML = "Weather in " + data.name
         document.querySelector(".temp").innerHTML = Math.floor(data.main.temp) + " Cº."
         document.querySelector(".text-weather-mood").innerHTML = data.weather[0].description + "."
+        countryElement.setAttribute("src", apiCountryURL);
         document.querySelector(".text-humidity").innerHTML = data.main.humidity + " %."
         document.querySelector(".bx-cloud").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
     }catch {
